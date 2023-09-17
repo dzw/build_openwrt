5dc5778830384aff04e7517681f09a5b5e4a06a6
dzw <dzwwgy@163.com>
2023-09-17 16:06:03 +0800

    openwrt-dependent-dl

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,10 @@ echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf
 
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# git pull
+
+# mkdir dl
+# git clone –depth 1 git@gitee.com:kwill/openwrt-dependent-dl.git dl
